18f2af7e7a4d18d93a332f712fd8d9636789decf
Indhu <indhu@Indhu-Air.local>
2023-06-08 12:28:55 +0530

    deploymemt

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/iamindhu.github.io",
-  plugins: [react()],
+// export default defineConfig({
+//   base: "/iamindhu.github.io",
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/portfolio/'
+  }
+
+  return config
 })
